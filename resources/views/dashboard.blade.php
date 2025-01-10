a516f83cd3fc1a4b1b70480e1c02bd39302fedb4
devin <h1051211002@student.untan.ac.id>
2025-01-10 16:41:23 +0700

    adjusted for k3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -94,7 +94,7 @@
                     },
                     y: {
                         min: 4,
-                        max: 6,
+                        max: 6.5,
                         ticks: {
                             stepSize: 0.5
                         }
@@ -133,7 +133,7 @@
                     },
                     y: {
                         min: 4,
-                        max: 6,
+                        max: 6.5,
                         ticks: {
                             stepSize: 0.5
                         }
